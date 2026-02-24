19d05e766473e09bcf863938d64acd59c5b66fac
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-02-24 20:40:26 -0300

    sincrono VS assincrono

---
+++ sincrono_vs_assincrono.js
@@ -0,0 +1,13 @@
+//sincrono execulta linha a linha, e só vai para próxima linha quando a anterior finalizar a função dela.
+// Código SÌNCRONO
+let nome = 'Willian';
+let sobrenome = 'Santos Silva';
+let nome_completo = `Meu nome é: ${nome} ${sobrenome}`;
+console.log(nome_completo);
+
+//Código ASSÌNCRONO
+let nome2 = "Arthur";
+let sobrenome2 = 'Marques Barbosa';
+let temperatura = Maquininha.pegarTemperatura(); // ASSINCRONA não espera para dar continuidade.
+let completo = `${nome2} ${sobrenome2}`;
+console.log(completo);
